c40ce84fdaae7b9397b46aaff79be6cc3a220c33
myuon <ioi.joi.koi.loi@gmail.com>
2013-02-24 16:56:17 +0900

    make instances of Functor, Monad, Foldable and Recursive

--- CL.hs
+++ CL.hs
@@ -1,10 +1,6 @@
-
--- S = λxyz.(x z)(y z)
--- K = λxy.x
--- I = λx.x
-
 import Prelude hiding (foldr)
 import Control.Applicative
+import Control.Arrow
 import Data.Foldable
 
 data Expr a = App (Expr a) (Expr a) | Data a
@@ -33,11 +29,16 @@ instance Functor LamExpr where
 instance Foldable Expr where
   foldr f z (App x y) = foldr f (foldr f z y) x
   foldr f z (Data x) = f x z
+  
+class Recursive r where
+  recur :: (a -> b) -> (b -> b -> b) -> r a -> b
+
+instance Recursive Expr where
+  recur f g (App x y) = g (recur f g x) (recur f g y)
+  recur f _ (Data a) = f a
 
 expr :: (a -> String) -> Expr a -> String
-expr showA (App x y) = "(" ++ expr showA x ++ 
-                       " " ++ expr showA y ++ ")" 
-expr showA (Data a) = showA a
+expr showA = recur showA (\x y -> "(" ++ x ++ " " ++ y ++ ")")
 
 lambda :: Lambda -> String
 lambda (Lam x y) = "λx" ++ show x ++ "." ++ lambda y
@@ -51,81 +52,49 @@ ski (Data K) = "K"
 ski (Data I) = "I"
 
 ski2lambda :: SKI -> Lambda
-ski2lambda = apply . replace
+ski2lambda = apply . fmap replace
 
-replace :: SKI -> Expr Lambda
-replace x = x >>= skiReplace
-  where
-    skiReplace :: CompSKI -> Expr Lambda
-    skiReplace S = Data (
-      Lam 1 (Lam 2 (Lam 3 (LamExpr (App
-                                    (App (Data $ LamValue 1) (Data $ LamValue 3))
-                                    (App (Data $ LamValue 2) (Data $ LamValue 3)))))))
-    skiReplace K = Data (Lam 1 (Lam 2 (LamExpr (Data $ LamValue 1))))
-    skiReplace I = Data (Lam 1 (LamExpr (Data $ LamValue 1)))
+replace :: CompSKI -> Lambda
+replace S = 
+  Lam 1 (Lam 2 (Lam 3 (LamExpr (App
+                                (App (Data $ LamValue 1) (Data $ LamValue 3))
+                                (App (Data $ LamValue 2) (Data $ LamValue 3))))))
+replace K = Lam 1 (Lam 2 (LamExpr (Data $ LamValue 1)))
+replace I = Lam 1 (LamExpr (Data $ LamValue 1))
 
 apply :: Expr Lambda -> Lambda
 apply = concatExpr . alphaSimple . beta . alphaNumber
 
 alphaNumber :: Expr Lambda -> Expr Lambda
-alphaNumber = fst . alphaNum 1 1
-
-alphaNum :: Int -> Int -> Expr Lambda -> (Expr Lambda, Int)
-alphaNum n s (App x y) = (App x' y', n'')
-  where
-    (x', n') = alphaNum n 1 x
-    (y', n'') = alphaNum n' n' y
-alphaNum n s (Data x) = (Data dat, n')
-  where
-    (dat, n') = number s [n-1..] x
-    
-    number :: Int -> [Int] -> Lambda -> (Lambda, Int)
-    number m dic (Lam l ls) = (Lam (dic !! l) lam, m')
-      where
-        (lam, m') = number (m+1) dic ls
-    number m dic (LamExpr e) = (LamExpr exp, m)
-      where
-        (exp, m') = numberExpr (m+1) dic e
-    
-    numberExpr :: Int -> [Int] -> Expr Lambda -> (Expr Lambda, Int)
-    numberExpr n dic (App x y) = (App x' y', n'')
-      where
-        (x', n') = numberExpr n dic x
-        (y', n'') = numberExpr n' dic y
-    numberExpr n dic (Data (LamValue x)) = (Data $ LamValue (dic !! x), n)
-    numberExpr n dic (Data x) = (Data $ x', n')
-      where
-        (x', n') = number n dic x
+alphaNumber = fst . alpha True 1 1
 
 alphaSimple :: Expr Lambda -> Expr Lambda
-alphaSimple = fst . alphaNum' 1 1
+alphaSimple = fst . alpha False 1 1
 
-alphaNum' :: Int -> Int -> Expr Lambda -> (Expr Lambda, Int)
-alphaNum' n s (App x y) = (App x' y', n'')
+alpha :: Bool -> Int -> Int -> Expr Lambda -> (Expr Lambda, Int)
+alpha isN n _ (App x y) = (App x' y', n'')
   where
-    (x', n') = alphaNum' n 1 x
-    (y', n'') = alphaNum' n' n' y
-alphaNum' n s (Data x) = (Data dat, n')
+    (x', n') = alpha isN n 1 x
+    (y', n'') = alpha isN n' n' y
+alpha isN n s (Data x) = (Data dat, n')
   where
     (dat, n') = number s [n-1..] x
     
+    pick :: a -> a -> a
+    pick a b = if isN then a else b
+    
     number :: Int -> [Int] -> Lambda -> (Lambda, Int)
-    number m dic (Lam l ls) = (Lam (dic !! m) lam, m')
-      where
-        (lam, m') = number (m+1) dic ls
-    number m dic (LamExpr e) = (LamExpr exp, m)
-      where
-        (exp, m') = numberExpr 1 dic e
+    number m dic (Lam l ls) = first (Lam (dic !! pick l m)) $ number (m+1) dic ls
+    number m dic (LamExpr e) = first LamExpr $ numberExpr (pick (m+1) 1) dic e
+    number _ _ _ = undefined
     
     numberExpr :: Int -> [Int] -> Expr Lambda -> (Expr Lambda, Int)
     numberExpr n dic (App x y) = (App x' y', n'')
       where
         (x', n') = numberExpr n dic x
         (y', n'') = numberExpr n' dic y
-    numberExpr n dic (Data (LamValue x)) = (Data $ LamValue (dic !! n), n)
-    numberExpr n dic (Data x) = (Data $ x', n')
-      where
-        (x', n') = number n dic x
+    numberExpr n dic (Data (LamValue x)) = (Data $ LamValue (dic !! pick x n), n)
+    numberExpr n dic (Data x) = first Data $ number n dic x
 
 concatExpr :: Expr Lambda -> Lambda
 concatExpr (Data x) = x
@@ -136,12 +105,12 @@ beta (App (Data x) (Data y)) = Data (y `subst` x)
   where
     subst :: Lambda -> Lambda -> Lambda
     subst x (Lam n ns) = x `substExpr` ns $ n
-    subst x _ = undefined
+    subst _ _ = undefined
     
     substExpr :: Lambda -> Lambda -> Int -> Lambda
     substExpr x (LamExpr e) n = LamExpr $ update x n e
     substExpr x (Lam m ms) n = Lam m (substExpr x ms n)
-    substExpr x _ n = undefined
+    substExpr _ _ _ = undefined
     
     update :: Lambda -> Int -> Expr Lambda -> Expr Lambda
     update lam n (App x y) = beta $ App (update lam n x) (update lam n y)
@@ -155,9 +124,8 @@ aSKI :: SKI
 aSKI = App (App (Data S) (Data K)) (Data K)
 
 aLambda :: Lambda
-aLambda = Lam 1 (Lam 2 (LamExpr (App (Data $ LamValue 1) (Data $ LamValue 2))))
+aLambda = Lam 1 (Lam 2 (Lam 3 (LamExpr (App
+                                (App (Data $ LamValue 1) (Data $ LamValue 3))
+                                (App (Data $ LamValue 2) (Data $ LamValue 3))))))
 
-main = putStrLn $ lambda . ski2lambda $ aSKI
--- ((SK)K)
--- ((λx1.λx2.λx3.((x1 x3) (x2 x3)) λx4.λx5.x4) λx6.λx7.x6)
--- λx1.x1
+main = putStrLn $ lambda $ ski2lambda aSKI
